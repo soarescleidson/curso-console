9f852f01d9f93fe9a16e0adf86f55537f5ceac81
soarescleidson <cleidson-santos@bol.com.br>
2016-07-11 22:02:10 -0300

    curso

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,73 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            int opcao, operacao;
+            string cliente = "", produto = "";
+            do{
+            Console.Clear();
+            Console.WriteLine("1-Cliente");
+            Console.WriteLine("2-produto");
+            Console.WriteLine("3-Sair");
+            opcao = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("1- Cadastrar Cliente ");
+                    Console.WriteLine("2- Consultar Cliente");
+                    break;
+                case 2:
+                    Console.WriteLine("1-cadastrar produto");
+                    Console.WriteLine("2-Consultar produto");
+                    break;
+                case 3:
+                    Console.WriteLine("Saindo...");
+                    break;
+                default:
+                    Console.WriteLine("Operacao Invalida!");
+                    break;
+            }
+            if ((opcao.Equals(1)))
+            {
+                operacao = Convert.ToInt32(Console.ReadLine());
+                if (operacao.Equals(1))
+                {
+                    Console.WriteLine("informe o nome:");
+                    cliente = Console.ReadLine();
+                }
+                else if ((operacao.Equals(2)))
+                {
+                    Console.WriteLine("nome do Cliente:" + cliente);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Opçao Invalida!");
+                }
+            }
+            else if ((opcao.Equals(2)))
+            {
+                operacao = Convert.ToInt32(Console.ReadLine());
+                if (operacao.Equals(1))
+                {
+                    Console.Write("Informe o nome:");
+                    produto = Console.ReadLine();
+                }
+                else if ((operacao.Equals(2)))
+                {
+                    Console.Write("Descriçao:" + produto);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("opcao invalida!");
+                }
+            }
+
+        } while (opcao != 3);
+
 
         }
     }
 }
+ 
